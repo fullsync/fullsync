19c9747b17ecfdbb150131d52575b2fde12e2725
Michele Aiello <mikyaiello@users.sourceforge.net>
2005-05-30 22:45:21 +0000

    - added a scrolling text for credits in the about box

--- source/net/sourceforge/fullsync/ui/AboutDialog.java
+++ source/net/sourceforge/fullsync/ui/AboutDialog.java
@@ -1,10 +1,16 @@
 package net.sourceforge.fullsync.ui;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import net.sourceforge.fullsync.ExceptionHandler;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.program.Program;
@@ -34,9 +40,31 @@ public class AboutDialog extends org.eclipse.swt.widgets.Dialog {
 	private Label labelPicture;
 	private Label labelSeparator;
 	private Composite compositeBottom;
+	private Label label2;
+	private Label labelThanks;
+	private Composite composite1;
 	private Button buttonOk;
 	private Button buttonWebsite;
 
+	private static final String[] specialThanks = {"", "", "", "Credits", 
+			"", 
+			"Localizations",
+			"German: Jan Kopcsek",
+			"Italian: Michele Aiello",
+			"Spanish: Angela Menghini",
+			"French: ... ...",
+			"",
+			"Testing",
+			"Antonio Denegri",
+			"",
+			"",
+			""};
+	
+	private static final long delay = 750;
+	
+	private int stIndex = 0;
+	private Timer stTimer;
+	
 	public AboutDialog(Shell parent, int style) 
 	{
 		super(parent, style);
@@ -47,6 +75,14 @@ public class AboutDialog extends org.eclipse.swt.widgets.Dialog {
 			Shell parent = getParent();
 			dialogShell = new Shell(parent, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
 
+			dialogShell.addDisposeListener(new DisposeListener() {
+				public void widgetDisposed(DisposeEvent arg0) {
+					if (stTimer != null) {
+						stTimer.cancel();
+					}
+				}
+			});
+			
 			GridLayout dialogShellLayout = new GridLayout();
 			dialogShell.setLayout(dialogShellLayout);
 			dialogShellLayout.verticalSpacing = 0;
@@ -54,7 +90,7 @@ public class AboutDialog extends org.eclipse.swt.widgets.Dialog {
 			dialogShellLayout.marginWidth = 0;
 			dialogShellLayout.horizontalSpacing = 0;
 			dialogShell.setText(Messages.getString("AboutDialog.About_FullSync")); //$NON-NLS-1$
-			dialogShell.setSize(308, 406);
+			dialogShell.setSize(308, 458);
             {
                 labelPicture = new Label(dialogShell, SWT.NONE);
                 GridData labelPictureLData = new GridData();
@@ -63,6 +99,7 @@ public class AboutDialog extends org.eclipse.swt.widgets.Dialog {
                 labelPictureLData.horizontalAlignment = GridData.FILL;
                 labelPictureLData.verticalAlignment = GridData.FILL;
                 labelPicture.setLayoutData(labelPictureLData);
+                labelPicture.setSize(300, 342);
         		labelPicture.setImage( GuiController.getInstance().getImage( "About.png" ) ); //$NON-NLS-1$
             }
             {
@@ -72,6 +109,58 @@ public class AboutDialog extends org.eclipse.swt.widgets.Dialog {
                 labelSeparatorLData.grabExcessHorizontalSpace = true;
                 labelSeparator.setLayoutData(labelSeparatorLData);
             }
+			{
+				composite1 = new Composite(dialogShell, SWT.NONE);
+				GridLayout composite1Layout = new GridLayout();
+				composite1Layout.makeColumnsEqualWidth = true;
+				GridData composite1LData = new GridData();
+				composite1LData.horizontalSpan = 2;
+				composite1LData.horizontalAlignment = GridData.FILL;
+				composite1LData.heightHint = 50;
+				composite1.setLayoutData(composite1LData);
+				composite1.setLayout(composite1Layout);
+				composite1.setBackground(new Color(null, 255, 255, 255));
+				{
+					labelThanks = new Label(composite1, SWT.WRAP);
+					GridData labelThanksLData = new GridData();
+					labelThanksLData.grabExcessHorizontalSpace = true;
+					labelThanksLData.horizontalAlignment = GridData.FILL;
+					labelThanksLData.verticalAlignment = GridData.FILL;
+					labelThanksLData.grabExcessVerticalSpace = true;
+					labelThanks.setLayoutData(labelThanksLData);
+					labelThanks.setBackground(new Color(null, 255, 255, 255));
+					labelThanks.setText("");
+					labelThanks.setAlignment(SWT.CENTER);
+					stTimer = new Timer(false);
+					stTimer.scheduleAtFixedRate(new TimerTask() {
+						public void run() {
+							Display display = Display.getDefault();
+							display.syncExec(new Runnable() {
+								public void run() {
+									int firstLine = (stIndex) % specialThanks.length;
+									int secondLine = (stIndex+1) % specialThanks.length;
+									int thirdLine = (stIndex+2) % specialThanks.length;
+
+									labelThanks.setText(specialThanks[firstLine]+'\n'+
+											specialThanks[secondLine]+'\n'+
+											specialThanks[thirdLine]);
+									stIndex++;
+									stIndex %= specialThanks.length;
+								}	
+							});
+						}
+					}, delay, delay);
+				}
+			}
+			{
+				label2 = new Label(dialogShell, SWT.SEPARATOR | SWT.HORIZONTAL);
+				GridData label2LData = new GridData();
+				label2LData.heightHint = 3;
+				label2LData.grabExcessHorizontalSpace = true;
+				label2LData.horizontalAlignment = GridData.FILL;
+				label2.setLayoutData(label2LData);
+				label2.setText("label2");
+			}
             {
                 compositeBottom = new Composite(dialogShell, SWT.NONE);
                 GridLayout compositeBottomLayout = new GridLayout();
