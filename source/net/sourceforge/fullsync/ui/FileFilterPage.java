9df9727a3b0e1ad0d6ad4621078e2f2b2a608001
Michele Aiello <mikyaiello@users.sourceforge.net>
2006-02-07 12:10:34 +0000

    - Minor changes in the FileFilter

--- source/net/sourceforge/fullsync/ui/FileFilterPage.java
+++ source/net/sourceforge/fullsync/ui/FileFilterPage.java
@@ -14,6 +14,21 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 
+
+/**
+* This code was generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* *************************************
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED
+* for this machine, so Jigloo or this code cannot be used legally
+* for any corporate or commercial purpose.
+* *************************************
+*/
 /**
  * @author Michele Aiello
  */
