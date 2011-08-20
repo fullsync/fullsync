5bb39dae6250fd5ea6f7bae0019b16cbdebdc34b
Christoph Obexer <cobexer@users.sourceforge.net>
2011-08-20 19:40:01 +0000

    cleanup.sh: also remove empty messages from message files, otherwise the english fallback will not work

--- cleanup.sh
+++ cleanup.sh
@@ -6,8 +6,8 @@
 
 for msg in source/net/sourceforge/fullsync/ui/messages*.properties;
 do
-	echo "removing comments of $msg..."
+	echo "removing comments and empty messages of $msg..."
 	#sed -i "s/^#.*//" "$msg"
-	cat "$msg" | grep -v "^#" > "$msg._tmp"
+	cat "$msg" | grep -v "^#" | grep -v "[A-Za-z0-9\.]=$" > "$msg._tmp"
 	mv -f "$msg._tmp" "$msg"
 done
