64c1d820ff52267a50e3bd92f9f2a68b461b3011
Christoph Obexer <cobexer@users.sourceforge.net>
2011-12-03 18:53:24 +0000

    Delete obsolete files / folders

--- resources/bin/fullsync.bat
+++
@@ -1,1 +0,0 @@
-@start javaw -classpath lib/fullsync.jar;lib/commons-cli.jar;lib/commons-net.jar;lib/log4j.jar;lib/utils.jar;lib/commons-configuration.jar;lib/swt.jar;lib/commons-beanutils.jar;lib/xml-apis.jar;lib/j2ssh-core.jar;lib/commons-collections.jar;lib/jakarta-or.jar;lib/commons-logging.jar;lib/XercesImpl.jar net.sourceforge.fullsync.cli.Main
