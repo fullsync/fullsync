5c2e04e23c7cace91e0ccd75d4cd578ff1f3d50b
Christoph Obexer <cobexer@users.sourceforge.net>
2012-06-17 21:22:13 +0000

    Launcher Linux: tell the user if java could not be found using xmessage
    xmessage is ugly, seriously ugly but it should be available on any
    system that has an X server...

--- resources/FullSync.sh
+++ resources/FullSync.sh
@@ -7,9 +7,14 @@ REAL_FULLSYNC_DIR=`readlink -f ${FULLSYNC_DIR}`
 JAVA_BIN=`which java`
 
 if [ "x$JAVA_BIN" = "x" ]; then
-	echo "Java could not be found! Please install Java first!"
+	MESSAGE="Java could not be found! Please install a Java runtime first!"
+	echo "$MESSAGE"
+	xmessage -center --text "$MESSAGE"
 	exit 1
 else
 	cd "${REAL_FULLSYNC_DIR}"
 	exec "${JAVA_BIN}" ${JAVA_OPTS} -jar "${REAL_FULLSYNC_DIR}/launcher.jar" "$@"
+	STATUS=$?
+	echo "Error running FullSync, please check your Java / FullSync installation!"
+	exit $STATUS
 fi
