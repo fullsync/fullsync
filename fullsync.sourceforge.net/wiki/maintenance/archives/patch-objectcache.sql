c82ba07d05712854640a4515e41106f6dacb1ba9
Obexer Christoph <cobexer@gmail.com>
2013-01-27 14:49:00 +0100

    drop the unused wiki from the website

--- fullsync.sourceforge.net/wiki/maintenance/archives/patch-objectcache.sql
+++
@@ -1,8 +0,0 @@
--- For a few generic cache operations if not using Memcached
-CREATE TABLE objectcache (
-  keyname char(255) binary not null default '',
-  value mediumblob,
-  exptime datetime,
-  unique key (keyname),
-  key (exptime)
-);
