2a97e1c76ad7f2892216b2bb7e5fcea5093c209f
Obexer Christoph <cobexer@users.sourceforge.net>
2013-02-08 21:08:08 +0100

    add new screenshots to the website

--- fullsync.sourceforge.net/screenshots.php
+++ fullsync.sourceforge.net/screenshots.php
@@ -1,21 +1,60 @@
 <?php
-	include( "html.php" );
-	HtmlHeader( "Screenshots" );
-?>
-	<h1>Screenshots</h1>
-	<img src="pictures/2004-12-12-MainWindow.png"><br>
-	The main window with the new profile list. You can still switch back<br>
-	to the table if you wish, but it has a bit less features.<br><br><br>
-	<img src="pictures/2004-11-25-TaskDecision.png"><br>
-	If you select an interactive synchronization process<br>
-	you will get this screen to see and change the actions<br>
-	that will be performed.<br/><br/><br/>
-	<img src="pictures/2004-11-25-Profile_Details.png"><br>
-	This dialog is used to configure the profiles. Here you specify<br>
-	what, how and when should be synchronized.<br><br><br>
-	<img src="pictures/2004-11-25-Profile_Scheduler.png"><br>
-	Here you can specify when a profile is executed automatically.<br>
-	You can either choose an interval, a crontab-like scheduling<br>
-	(you can see in the picture) or none.<br><br><br>
-<?php
-	HtmlFooter();
+include("html.php");
+HtmlHeader("Screenshots");
+define('SCREENSHOTS_DIR', "screenshots");
+
+function getScreesnhots($major, $minor, $patch) {
+	$screenshots = array();
+	$dir = SCREENSHOTS_DIR . '/' . "$major.$minor.$patch/";
+	$d = dir($dir);
+	if (false !== $d) {
+		while (false !== ($entry = $d->read())) {
+			if (preg_match('/\.(png|jpg|jpeg|gif)$/i', $entry)) {
+				$content = '';
+				if (file_exists($dir . $entry . '.html')) {
+					$content = file_get_contents($dir . $entry . '.html');
+				}
+				$screenshots[$dir . $entry] = $content;
+			}
+		}
+		$d->close();
+	}
+	return $screenshots;
+}
+
+
+$screenshots = array();
+
+$d = dir(SCREENSHOTS_DIR);
+if (false !== $d) {
+	while (false !== ($entry = $d->read())) {
+		if (preg_match('/^(\d+)\.(\d+)\.(\d+)$/', $entry, $version)) {
+			$screenshots[] = array(
+				'major' => $version[1],
+				'minor' => $version[2],
+				'patch' => $version[3],
+				'screenshots' => getScreesnhots($version[1], $version[2], $version[3])
+			);
+		}
+	}
+	$d->close();
+}
+usort($screenshots, 'versionComparator');
+
+echo "<h2>Screenshots</h2>";
+
+foreach($screenshots as $version) {
+	if (empty($version['screenshots'])) {
+		continue;
+	}
+	echo "<h3>Version ${version['major']}.${version['minor']}.${version['patch']}</h3>";
+	foreach($version['screenshots'] as $s => $desc) {
+		echo '<img src="' . ${s} . '" /><br />';
+		if ($desc) {
+			echo $desc . "<br />";
+		}
+		echo "<br />";
+	}
+}
+
+HtmlFooter();
