21806df33627cc5921d3107a05bcdd0877b94c5b
Obexer Christoph <cobexer@users.sourceforge.net>
2014-05-17 19:53:28 +0200

    some work on the SyncActionTaskGenerator

--- source/net/sourceforge/fullsync/pipeline/SyncActionGenerator.java
+++ source/net/sourceforge/fullsync/pipeline/SyncActionGenerator.java
@@ -19,7 +19,11 @@
  */
 package net.sourceforge.fullsync.pipeline;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import net.sourceforge.fullsync.Action;
 import net.sourceforge.fullsync.BufferUpdate;
@@ -33,55 +37,18 @@ public class SyncActionGenerator {
 	private final SmartQueue<Task> queue;
 	private final SyncActionGeneratorTask sourceTask;
 	private final SyncActionGeneratorTask destinationTask;
-	private boolean sourceRunning;
-	private boolean destinationRunning;
-	private List<File> sourceList;
-	private List<File> destinationList;
-	private File currentSourceParent;
-	private File currentDestinationParent;
-	SyncActionGenerator(ProfileSyncTask _task, SmartQueue<File> _sourceQueue, SmartQueue<File> _destinationQueue) {
+	SyncActionGenerator(TaskletWorkNotificationTarget _workNotificationTarget, SmartQueue<File> _sourceQueue, SmartQueue<File> _destinationQueue) {
 		queue = new SmartQueue<Task>();
-		sourceTask = new SyncActionGeneratorTask(_task, this, Origin.Source, _sourceQueue);
-		destinationTask = new SyncActionGeneratorTask(_task, this, Origin.Destination, _destinationQueue);
-		sourceRunning = true;
-		destinationRunning = true;
+		sourceTask = new SyncActionGeneratorTask(_workNotificationTarget, this, Origin.Source, _sourceQueue);
+		destinationTask = new SyncActionGeneratorTask(_workNotificationTarget, this, Origin.Destination, _destinationQueue);
+		sourceTask.setOther(destinationTask);
+		destinationTask.setOther(sourceTask);
 	}
 
-	synchronized void doProcessItem(Origin origin, File item) {
-		if (null == currentSourceParent && Origin.Source == origin) {
-			currentSourceParent = item;
-		}
-		if (null == currentDestinationParent && Origin.Destination == origin) {
-			currentDestinationParent = item;
-		}
-		switch(origin) {
-			case Destination:
-				break;
-			case Source:
-				break;
-			default:
-				break;
-		}
-		//FIXME: make an actual decision
-		if (Origin.Source == origin) {
-			Task t = new Task(item, null, new State(State.Orphan, Location.Source), new Action[] { new Action(Action.Add, Location.Destination, BufferUpdate.None, "") });
-			queue.offer(t);
-		}
-	}
-
-	synchronized void notifyEnd(Origin origin) {
-		switch (origin) {
-			case Destination:
-				destinationRunning = false;
-				break;
-			case Source:
-				sourceRunning = false;
-				break;
-		}
-		if (false == sourceRunning && false == destinationRunning) {
-			//TODO: flush parent lookup lists
-			queue.shutdown();
-		}
+	public void doDecideOne(File srcParent, File src, File dstParent, File dst) {
+		//FIXME: make an actual decision (state decider, check source / destination, ...)
+		Task t = new Task(src, dst, new State(State.NodeInSync, Location.Both), new Action[] { new Action(Action.Nothing, Location.Destination, BufferUpdate.None, "") });
+		getOutput().offer(t);
 	}
 
 	public SmartQueue<Task> getOutput() {
@@ -95,6 +62,24 @@ public class SyncActionGenerator {
 	public SyncTasklet<File, Task> getDestinationTask() {
 		return destinationTask;
 	}
+
+	public synchronized void pause() {
+		getOutput().pause();
+	}
+
+	public synchronized void resume() {
+		getOutput().resume();
+	}
+
+	public synchronized void cancel() {
+		getOutput().cancel();
+	}
+
+	public synchronized void notifyEnd() {
+		if (sourceTask.hasEnded() && destinationTask.hasEnded()) {
+			getOutput().shutdown();
+		}
+	}
 }
 
 enum Origin {
@@ -102,48 +87,196 @@ enum Origin {
 	Destination
 }
 
-class OriginState {
-	private boolean running;
-	private List<File> backlog;
-	private File currentParent;
-
-	public void processItem(File item) {
-		if (null == currentParent) {
-			currentParent = item;
-		}
-		item.getPath();
-	}
-
-}
-
 class SyncActionGeneratorTask extends SyncTasklet<File, Task> {
-	private SyncActionGenerator generator;
-	private Origin origin;
-	public SyncActionGeneratorTask(ProfileSyncTask _task, SyncActionGenerator _generator, Origin _origin, SmartQueue<File> _inputQueue) {
-		super(_task, _inputQueue);
+	enum ParentState {
+		Pending,
+		ParentsMatch,
+		ParentsDiffer
+	}
+	private class WorkingSet {
+		private HashMap<String, File> map = new HashMap<String, File>();
+		private boolean isActive = true;
+
+		public void put(String name, File file) {
+			map.put(name, file);
+		}
+
+		public File remove(String name) {
+			return map.remove(name);
+		}
+
+		public boolean isEmpty() {
+			return map.isEmpty();
+		}
+
+		public boolean isActive() {
+			return isActive;
+		}
+
+		public void setActive(boolean active) {
+			isActive = active;
+		}
+
+		public Set<String> keySet() {
+			return map.keySet();
+		}
+	}
+	private final SyncActionGenerator generator;
+	private final Origin origin;
+	private final List<File> backlog;
+	private final List<File> possibleParents;
+	private File currentParent;
+	private final HashMap<String, WorkingSet> workingSetMap;
+	private SyncActionGeneratorTask other;
+	private ParentState parentState;
+	private volatile boolean running;
+	public SyncActionGeneratorTask(TaskletWorkNotificationTarget _workNotificationTarget, SyncActionGenerator _generator, Origin _origin, SmartQueue<File> _inputQueue) {
+		super(_workNotificationTarget, _inputQueue);
+		generator = _generator;
 		origin = _origin;
+		backlog = new LinkedList<File>();
+		possibleParents = new LinkedList<File>();
+		currentParent = null;
+		workingSetMap = new HashMap<String, WorkingSet>();
+		parentState = ParentState.Pending;
+		running = true;
 	}
 
 	@Override
 	protected void processItem(File item) throws Exception {
-		generator.doProcessItem(origin, item);
+		synchronized (generator) {
+			if (ParentState.Pending == parentState) {
+				currentParent = item.getParent();
+				parentState = ParentState.ParentsMatch;
+			}
+			if (item.getParent() != currentParent) {
+				backlog.add(item);
+				parentState = ParentState.ParentsDiffer;
+				WorkingSet workingSet = workingSetMap.get(currentParent.getPath());
+				if (null != workingSet) {
+					workingSet.setActive(false);
+				}
+			}
+			else {
+				File twin = other.poll(currentParent.getPath(), item.getName());
+				if (null != twin) {
+					doDecideOne(item, twin);
+				}
+				else {
+					String parentPath = item.getParent().getPath();
+					WorkingSet workingSet = workingSetMap.get(parentPath);
+					if (null == workingSet) {
+						workingSet = new WorkingSet();
+						workingSetMap.put(parentPath, workingSet);
+					}
+					workingSet.put(item.getName(), item);
+				}
+			}
+			/*
+			if (ParentState.ParentsDiffer == parentState && ParentState.ParentsDiffer == other.getState()) {
+				nextWorkingSet(true);
+			}
+			*/
+		}
+	}
+/*
+	private void nextWorkingSet(boolean notifyOther) {
+		for (Entry<String, File> entry : workingSet.entrySet()) {
+			doDecideOne(entry.getValue(), other.poll(entry.getKey()));
+		}
+		workingSet.clear();
+		other.nextWorkingSet(false);
+		if (!backlog.isEmpty()) {
+			currentParent = backlog.get(0).getParent();
+			while (!backlog.isEmpty() && backlog.get(0).getParent() == currentParent) {
+				File item = backlog.remove(0);
+				workingSet.put(item.getName(), item);
+			}
+			parentState = ParentState.ParentsMatch;
+		}
+		else {
+			currentParent = null;
+			parentState = ParentState.Pending;
+		}
+	}
+*/
+	private void doDecideOne(File item, File twin) {
+		if (Origin.Source == origin) {
+			generator.doDecideOne(currentParent, item, other.getParent(), twin);
+		}
+		else {
+			generator.doDecideOne(other.getParent(), twin, currentParent, item);
+		}
+	}
+
+	public void setOther(SyncActionGeneratorTask _other) {
+		other = _other;
+	}
+
+	private ParentState getState() {
+		return parentState;
+	}
+
+	private File poll(String path, String name) {
+		File f = null;
+		WorkingSet workingSet = workingSetMap.get(path);
+		if (null != workingSet) {
+			f = workingSet.remove(name);
+			if (workingSet.isEmpty()) {
+				workingSetMap.remove(path);
+			}
+		}
+		return f;
+	}
+
+	private File getParent() {
+		return currentParent;
 	}
 
 	@Override
 	public void pause() {
+		generator.pause();
 	}
 
 	@Override
 	public void resume() {
+		generator.resume();
 	}
 
 	@Override
 	public void cancel() {
+		synchronized (generator) {
+			generator.cancel();
+			running = false;
+		}
+	}
+
+	private void flushWorkingset(String path, WorkingSet workingSet) {
+		for (String name : workingSet.keySet()) {
+			doDecideOne(workingSet.remove(name), other.poll(path, name));
+		}
 	}
 
 	@Override
 	protected void cleanup() {
-		super.cleanup();
-		generator.notifyEnd(origin);
+		synchronized (generator) {
+			boolean otherSideEnded = other.hasEnded();
+			parentState = ParentState.ParentsDiffer;
+			for (Map.Entry<String, WorkingSet> entry : workingSetMap.entrySet()) {
+				entry.getValue().setActive(false);
+				if (otherSideEnded) {
+					flushWorkingset(entry.getKey(), entry.getValue());
+				}
+			}
+			running = false;
+			if (otherSideEnded) {
+				other.cleanup();
+			}
+			generator.notifyEnd();
+		}
+	}
+
+	public boolean hasEnded() {
+		return !running;
 	}
 }
---
+++ tests/net/sourceforge/fullsync/pipeline/SyncActionGeneratorTest.java
@@ -0,0 +1,109 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * For information about the authors of this project Have a look
+ * at the AUTHORS file in the root of this project.
+ */
+package net.sourceforge.fullsync.pipeline;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+import net.sourceforge.fullsync.Action;
+import net.sourceforge.fullsync.Location;
+import net.sourceforge.fullsync.Task;
+import net.sourceforge.fullsync.fs.File;
+import net.sourceforge.fullsync.rules.filefilter.TestNode;
+import net.sourceforge.fullsync.util.SmartQueue;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SyncActionGeneratorTest {
+	File root = new TestNode("", null, true, true, 0, 0);
+
+	Thread sourceThread;
+	Thread destinationThread;
+	SmartQueue<File> sourceQueue;
+	SmartQueue<File> destinationQueue;
+	SyncActionGenerator generator;
+	long now;
+
+	protected long getLastModified() {
+		Calendar cal = Calendar.getInstance(TimeZone.getDefault());
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		Date d = cal.getTime();
+		return d.getTime();
+	}
+
+	@Before
+	public void prepare() {
+		sourceQueue = new SmartQueue<File>();
+		destinationQueue = new SmartQueue<File>();
+		TestNotificationTarget workNotificationTarget = new TestNotificationTarget();
+		generator = new SyncActionGenerator(workNotificationTarget, sourceQueue, destinationQueue);
+		sourceThread = new Thread(generator.getSourceTask());
+		sourceThread.start();
+		destinationThread = new Thread(generator.getDestinationTask());
+		destinationThread.start();
+		now = getLastModified();
+	}
+
+	@After
+	public void teardown() throws InterruptedException {
+		sourceQueue.cancel();
+		destinationQueue.cancel();
+		sourceThread.join();
+		destinationThread.join();
+	}
+
+	protected void verifyDecisions(String[] srcFiles, String[] dstFiles, String[] tasks) {
+
+	}
+
+	@Test(timeout = 10000)
+	public void testGeneratesTasks() {
+		sourceQueue.offer(new TestNode("a", root, true, false, 5, now));
+		sourceQueue.shutdown();
+		destinationQueue.shutdown();
+		Task t = generator.getOutput().take();
+		Assert.assertNotNull(t);
+		Assert.assertFalse(t.getCurrentAction().isError());
+		Assert.assertTrue(t.getCurrentAction().getType() == Action.Add);
+		Assert.assertTrue(t.getCurrentAction().getLocation() == Location.Destination);
+		Assert.assertEquals("/a", t.getSource().getPath());
+	}
+}
+
+class TestNotificationTarget implements TaskletWorkNotificationTarget {
+	@Override
+	public void startWork() {
+	}
+
+	@Override
+	public void endWork() {
+	}
+
+	@Override
+	public void syncEnded() {
+	}
+}
