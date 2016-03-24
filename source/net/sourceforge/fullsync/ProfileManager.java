5e0091ce7cfc0cc8891fbf6475a7737cc7f7134d
Obexer Christoph <cobexer@users.sourceforge.net>
2016-03-24 12:51:48 +0100

    if the profiles.xml file is empty, there is nothing to read... thus don't

--- source/net/sourceforge/fullsync/ProfileManager.java
+++ source/net/sourceforge/fullsync/ProfileManager.java
@@ -200,7 +200,7 @@ public class ProfileManager implements ProfileChangeListener, ScheduleTaskSource
 	public boolean loadProfiles(String profilesFileName)
 			throws SAXException, IOException, ParserConfigurationException, FactoryConfigurationError {
 		File file = new File(profilesFileName);
-		if (file.exists()) {
+		if (file.exists() && (file.length() > 0)) {
 			DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 			Document doc = builder.parse(file);
 
